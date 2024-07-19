cdbfabb5a54e53532d34b15ae3af5bfa8a82d91a
Dhruv Sharma <149249311+dhruv0050@users.noreply.github.com>
2024-07-19 20:43:49 +0530

    Add files via upload
    authentication and authorization--bcrypt and jwt

--- app.js
+++ app.js
@@ -1,47 +1,40 @@
 const express = require('express')
-const app = express()
-const usermodel = require('./usermodel')  //requiring the usermode.js file for CRUD operations
+const app = express();
+const cookieParser = require('cookie-parser')
+const bcrypt = require('bcrypt')
+const jwt= require('jsonwebtoken')
 
-app.get('/', (req , res) =>{
-    res.send("Hey MongoDB")
-})
+app.use(cookieParser());
 
-// app.get('/create', async (req , res) =>{                 //Creating user
-//     let createdUser = await usermodel.create({
-//         name: "Dhruv",
-//         email: "dhruvsh5467@gmail.com",
-//         username: "dh.ruv05"
-//     })
-//     res.send(createdUser)
-// })
-
-app.get('/create', async (req , res) =>{ 
-    let createdUser = await usermodel.create({
-        name: "Dhruvika",
-        email: "dhruvika5467@gmail.com",
-        username: "dh.ruvika"
+app.get('/', (req,res) =>{
+    let token = jwt.sign({email : "dhruv@gmail.com"},"secret_key");
+    console.log(token);
+    res.cookie("name", "dhruv123")                                              //Setting cookie
+    res.send("done")
+    console.log("cookie established") 
+    bcrypt.genSalt(10, (err,salt) =>{                                           //encrypting a password
+        bcrypt.hash("Mypassword", salt, (err,hash) =>{
+            console.log(hash);
+        })
     })
-    res.send(createdUser)
+    bcrypt.compare("Mypassword", "$2b$10$5FNmTPm0qgqPD70pWX9E4Oucj7U9de.kqyevOzaJOr5X1aWvjt28e",(err,result)=>{  
+        console.log(result);                                         //THis is used to compare a hash with password:
+                                                                     //If result==true =>matched else  different 
+    })               
+});
+app.get('/read', (req,res) =>{
+    res.send("Checking Cookie ")               //Checking if cookie still remains there for other route
+    console.log("On read route ")           
+    console.log(req.cookies);                  //reading cookies [package used==  npm i cookie-parser]
 })
 
-app.get('/update', async (req , res)=> {
-    let updateuser= await usermodel.findOneAndUpdate({name:"Dhruvika"},{name:"Dhrushika"},{new:true})         //updating user
-    res.send(updateuser)
-})
+app.get('/jwt', (req,res) =>{
+    let token = jwt.sign({email : "dhruv@gmail.com"},"secret_key");              //JsonWebToken
+    res.cookie("token",token);
+    res.send("COOKIE x JWT")
+    let data = jwt.verify(req.cookies.token,"secret_key");                      //logging data of token
+    console.log(data);
 
-app.get('/read', async (req , res)=> {
-    let user= await usermodel.find();         //Reading user
-    res.send(user)
-})
-
-app.get('/delete', async (req , res)=> {
-    let user= await usermodel.findOneAndDelete({name: "Dhrushika"});         //Deleting user
-    res.send(user)
-})
-app.get('/read', async (req , res)=> {
-    let user= await usermodel.find();         //Reading user
-    res.send(user)
-})
-
+});    
 
 app.listen(3000);
